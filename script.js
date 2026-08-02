b3244eba546e4c653196c3941d28cf85f6d5e5a7
addsadsfg123-ctrl <addsadsfg123@gmail.com>
2026-08-02 19:13:44 +0300

    Add card data structure for photo and caption

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const cards = [
+{
+image:"",
+text:"Совсем скоро здесь появится первое фото ❤️"
+}
+];
 
+const photo = document.getElementById("photo");
+const caption = document.getElementById("caption");
+
+photo.src = cards[0].image;
+caption.textContent = cards[0].text;
